7322bdd980d08a4bd08d22616fbc472f456b13df
juanrocha <aguilajk@gmail.com>
2020-03-03 14:29:14 +0100

    aesthetic updates on figures following Henrik requests

--- 200210_updatedFigures.R
+++ 200210_updatedFigures.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(network)
 library(ggnetwork)
 library(ggmap)
+library(RColorBrewer)
 
 # read data
 dat <- readxl::read_excel(
@@ -84,16 +85,37 @@ meetings <-  meetings %>%
     left_join(coords) #%>%
     #mutate(x = x + id)
 
+## Fix colors
+## AUSS, NP, Trident: not members (possible white or similar)
+# CAN, MHG, Skretting: Europe (Norway)
+# CPF, TUF: South East Asia (Thailand)
+# MNC, KK, NSK, MC: East Asia (Japan)
+# DW: East Asia (Korea) – not sure if separate from Japan or not… possibly a separate category from Japan.
+
+color <- vector(mode = "character", length = length(comps))
+color[comps %in% c("AUSS", "NP", "Trident")] <- brewer.pal(3, "Greys")
+color[comps %in% c("CAN", "MHG", "Skretting")] <- brewer.pal(3, "Reds")
+color[comps %in% c("CPF", "TUF")] <- brewer.pal(3, "Greens")
+color[comps %in% c("MNC", "KK", "NSK", "MC")] <- brewer.pal(4, "Blues")
+color[comps %in% c("DW")] <- "purple"
+color[comps %in% c("Scientists", "MD")] <- c("orange", "goldenrod")
+
+df_color <- tibble(
+    company = comps,
+    color = color
+) %>% arrange(company)
 # meetings$task_force[meetings$task_force == "Handover"] <- NA
 
 ## Stars plot: each network is plotted individually by date. Note there is more than one network per day!
 g <- meetings %>%
+    left_join(df_color) %>%
+    filter(company != "Scientist") %>%
     #filter(date > "2018-12-31") %>%
     ggplot(aes(x,y)) +
     geom_segment(xend = 0, yend = 0, color = "grey50", size = 0.25) + #irl_virtual
     geom_point(aes(size = n, fill = company, color = company), alpha = 0.75) + #irl_virtual
     facet_wrap(.~ date) + 
-    #scale_size_manual(breaks = c(1,5,10)) +
+    scale_color_manual(values = df_color$color) +
     theme_void(base_size = 6)  +
     theme(legend.position = "bottom", legend.direction = "horizontal") 
 
@@ -101,6 +123,8 @@ ggsave(g, filename = "timeline.png", device = "png", width = 7, height = 7, unit
 
 ## Time line
 g <- meetings %>%
+    left_join(df_color) %>%
+    filter(company != "Scientist") %>%
     mutate(year = lubridate::floor_date(date, "year"),
            alpha = ifelse(irl_virtual == "IRL", 1, 0.75)) %>%
     ggplot(aes(x = date, y = y)) +
@@ -113,7 +137,7 @@ g <- meetings %>%
                        guide = guide_legend(direction = "vertical")) + 
     scale_size("Number of people", breaks = c(1,5,10), 
                guide = guide_legend(direction = "vertical")) + 
-    scale_color_hue("Companies", aesthetics = c("color", "fill"), #option = "D",
+    scale_color_manual("Companies", aesthetics = c("color", "fill"), values = df_color$color,
        guide = guide_legend(direction = "vertical", nrow = 3) ) +
     geom_point(data = dat %>%
                    select(date, task_force, phase) %>%
@@ -175,12 +199,13 @@ companies_meeting <- companies_meeting %>%
 ## correcting one person without gender
 companies_meeting$gender[is.na(companies_meeting$gender)] <- "m"
 
-gender <- companies_meeting %>%
-    select(phase, actors, gender) %>% 
-    group_by(phase, gender) %>%
+gender <- companies_meeting %>% 
+    mutate(academic = ifelse(company == "Scientists", "academic", "corporate")) %>%
+    select(phase, actors, gender, academic) %>% 
+    group_by(phase, gender, academic) %>%
     unique() %>% 
     tally() %>% 
-    pivot_wider(id_cols = phase, values_from = n, 
+    pivot_wider(id_cols = c(phase, academic), values_from = n, 
                 names_from = gender, values_fill = list(n = 0)) %>%
     rename(male = m, female = f)
 
@@ -200,7 +225,10 @@ freq <- dat %>%
 
 p1 <- efforts %>% 
     left_join(carbon) %>% 
-    left_join(gender) %>% 
+    left_join(gender %>% 
+                  select(-academic) %>%
+                  mutate(male = sum(male), female = sum(female)) %>% 
+                  unique()) %>% 
     left_join(freq) %>% 
     select(-male, -female, -meetings, -months) %>%
     rename(`meetings per month` = freq, `carbon emissions` = carbon, `investment in $US` = money) %>%
@@ -213,12 +241,14 @@ p1 <- efforts %>%
 p2 <- gender %>%
     pivot_longer(cols = male:female, names_to = "gender", values_to = "participants") %>% 
     ggplot(aes(phase, participants)) +
-    geom_col(position = "stack", aes(fill = gender)) +
+    geom_col(position = "dodge", aes(fill = gender)) + 
+    #scale_fill_viridis_d(option = "A", aesthetics = "fill") +
+    facet_wrap(~academic, scales = "free") +
     theme_light(base_size = 8) +
-    theme(legend.position = c(0.2,0.9))
+    theme(legend.position = "right")
 
 library(patchwork)
-p1 + p2 + plot_layout(widths = c(3,1)) 
+p1 + p2 + plot_layout(widths = c(3,2)) 
 
 ggsave(filename = "descriptive_stats.png", device = "png", dpi = 600, width = 7, height = 2)
 ## add task forces
@@ -259,7 +289,8 @@ anim_save(filename = "SeaBos_200214.gif")
 
 
 actor_meeting <- companies_meeting %>%
-    select(date, actors, company) %>%
+    select(date, actors, company) %>% 
+    filter(company != "Scientists") %>%
     mutate(count = 1) %>%
     group_by(date, actors) %>% unique() %>%
     spread(key = actors, value = count, fill = 0)
@@ -267,6 +298,7 @@ actor_meeting <- companies_meeting %>%
 
 
 df_actors <- companies_meeting %>% 
+    filter(company != "Scientists") %>%
     group_by(date) %>% 
     mutate(companions = list(actors)) %>% 
     unnest() %>%
@@ -321,13 +353,14 @@ df_actors %>%
             group_by(phase) %>%
             select(phase, actors, x, y, company, meetings, gender) %>%
             unique(),
-        aes(x = x, y = y, color = company , shape = gender, fill = company, size = meetings), alpha = 1) +
-    scale_color_hue("Companies", aesthetics = c("color", "fill"), #option = "D",
-                    guide = guide_legend(direction = "vertical", nrow = 3) ) +
-    scale_size("Number of meetings",
-                      guide = guide_legend(direction = "vertical", ncol = 1)) +
-    scale_shape("Gender", 
-                       guide = guide_legend(direction = "vertical", ncol = 1)) +
+        aes(x = x, y = y, color = company, fill = company, size = meetings), alpha = 1) +
+    scale_color_manual("Companies", aesthetics = c("color", "fill"), #option = "D",
+                    guide = guide_legend(direction = "vertical", nrow = 3) ,
+                    values = df_color %>% filter(company != "Scientists") %>% pull(color)) +
+    scale_size("Number of meetings", breaks = c(50,100,200),
+                      guide = guide_legend(direction = "vertical", ncol = 1), range = c(0.5,3)) +
+    # scale_shape("Gender", 
+    #                    guide = guide_legend(direction = "vertical", ncol = 1)) +
     facet_wrap(.~phase) +
     theme_blank(base_size = 5) +
     theme(legend.position = "bottom")
